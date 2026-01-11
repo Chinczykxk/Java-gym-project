bd3a26ce0c5b04e813ab2de35ce69d79db422076
Mateusz Majewski <majaczek2821@gmail.com>
2026-01-11 19:21:52 +0100

    first version of main panel. Buttons in main menu have assigned actions

--- src/main/java/com/example/menu1/MeinController.java
+++ src/main/java/com/example/menu1/MeinController.java
@@ -1,23 +1,52 @@
 package com.example.menu1;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import java.io.IOException;
 
 public class MeinController {
 
     @FXML
-    private VBox myPane;
-
+    public Button login;
     @FXML
-    private Button login;
-
+    public Button register;
     @FXML
-    private Button register;
+    public Button exit;
 
+    // Open new windows method
+    public void openNewWindow(String fxmlFile, String title) {
+        try {
+            // Loading file fxml
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Błąd podczas otwierania okna: " + fxmlFile);
+            e.printStackTrace();
+        }
+    }
+    // function on click is calling function with the appropriate parameter to open loginPanel window
     @FXML
-    private Button exit;
+    public void onClickButtonLogin() {
+        openNewWindow("view-loginPanel.fxml", "Logowanie");
+    }
+    // function on click is calling function with the appropriate parameter to open registerPanel window
+    @FXML
+    public void onClickButtonRegister() {
+        openNewWindow("view-registerController.fxml", "Rejestracja");
+    }
+    // close app function
+    @FXML
+    public void onClickButtonExit() {
 
-
+        Platform.exit();
+        System.exit(0);
+    }
 }
