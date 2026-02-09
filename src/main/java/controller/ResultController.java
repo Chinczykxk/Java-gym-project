30e9946d53cff9ed5eeaff197cf2243a58d50def
Mateusz Majewski <majaczek2821@gmail.com>
2026-02-09 14:40:52 +0100

    working program after merge

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -4,11 +4,18 @@ import dao.PlanyDao;
 import dao.ExerciseDao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Exercise;
 import util.UserSession;
+
+import java.io.IOException;
 import java.util.List;
 
 public class ResultController {
@@ -114,9 +121,22 @@ public class ResultController {
         alert.showAndWait();
     }
 
-    @FXML private void handleClose() {
-        if (tvExercises.getScene() != null) {
-            tvExercises.getScene().getWindow().hide();
+
+    @FXML
+    public void handleClose(ActionEvent event) {
+        try {
+            String path = "/com/example/menu1/view-user-panel.fxml";
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(path));
+            if (loader.getLocation() == null) {
+                System.err.println("BŁĄD: Nie znaleziono pliku FXML pod: " + path);
+                return;
+            }
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
